9ea9a8aea7a7be4efd8a9226dbb26a427af3343e
Viet Anh <anhdv1508.ptit@gmail.com>
2025-11-21 17:33:22 +0700

    feature/auth-pages

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,27 @@
+// src/components/common/Input.tsx
+
+import React from 'react';
+// Nhập định nghĩa kiểu đã tạo
+import { InputProps } from '../../types/Input'; 
+// Nhập CSS Modules
+import styles from './Input.module.css';
+
+const Input: React.FC<InputProps> = ({ label, ...props }) => {
+  return (
+    <div className={styles.inputGroup}>
+      <label 
+        className={styles.label} 
+        htmlFor={props.id || props.name} // Thiết lập htmlFor liên kết với input
+      >
+        {label}
+      </label>
+      <input
+        className={styles.inputField}
+        // Truyền tất cả các props còn lại (type, placeholder, onchange, v.v.)
+        {...props} 
+      />
+    </div>
+  );
+};
+
+export default Input;
